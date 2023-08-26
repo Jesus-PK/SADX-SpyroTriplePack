b26f5a47abbdc9d7d8e43c0e0e157568f98032a7
Jesus-PK <134032385+Jesus-PK@users.noreply.github.com>
2023-08-26 23:00:59 +0200

    Add project files.

---
+++ SpyroTriplePack/pch.h
@@ -0,0 +1,25 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include "framework.h"
+
+#include "SADXModLoader.h"
+
+#include "FunctionHook.h"
+#include "Trampoline.h"
+#include "UsercallFunctionHandler.h"
+
+#include "ModelInfo.h"
+#include "LandTableInfo.h"
+#include "AnimationFile.h"
+
+#include "mod.h"
+
+#include "graphics.h"
+#include "levels.h"
+#include "leveltasks.h"
+#include "levelranks.h"
+#include "objects.h"
+#include "utils.h"
+
+#endif
