f14f9fe12cc06f04c5e0b40611e53ca13f19aea5
Jesus-PK <134032385+Jesus-PK@users.noreply.github.com>
2023-08-27 06:32:40 +0200

    Progress update: - Since I'm still organizing everything, I cannot try yet the mod. - Missing quite a few things like changing the names of the models / stuff the game is gonna load from my mod. - See how to organize titlecard folders. - Levelranks and missioncards, I might end up doing them in a global file due to how I edit them (Making one hook rather than 3) - Fix code from the gnorc cove exclusive objects.

--- SpyroTriplePack/mod.cpp
+++ SpyroTriplePack/mod.cpp
@@ -6,21 +6,31 @@ extern "C"
 {
 	__declspec(dllexport) void __cdecl Init(const char* path, const HelperFunctions& helperFunctions)
 	{
-		//	HelperFunctions pointer for external use:
+		//	Global Things:
 
 		HelperFunctionsGlobal = helperFunctions;
-
-		
-		//	Check Active Mods and Mod Loader API Version:
 
 		CheckActiveMods(helperFunctions);
 		CheckModLoaderVersion(helperFunctions);
 
-		//	Disable DC Conversion Levels:
+		WriteData<5>((void*)0x422C3E, 0x90); // Disable DC Conversion Twinkle Park
+		WriteData<5>((void*)0x422DEF, 0x90); // Disable DC Conversion Lost World
+		WriteData<5>((void*)0x422D84, 0x90); // Disable DC Conversion Sky Deck
+		
 
-		WriteData<5>((void*)0x422C3E, 0x90); // Twinkle Park
-		WriteData<5>((void*)0x422D84, 0x90); //	Sky Deck
-		WriteData<5>((void*)0x422DEF, 0x90); // Lost World
+		//	Gnorc Cove INIT:
+
+		
+
+
+		//	Town Square INIT:
+
+		
+
+		
+		//	Tree Tops INIT:
+
+		
 	}
 
 	__declspec(dllexport) void __cdecl OnFrame()
